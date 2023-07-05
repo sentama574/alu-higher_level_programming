9fd49e6a06fef80baa0d8bc67426bc226c48a2e7
sentama574 <sentamaj54@gmail.com>
2023-07-05 08:05:35 -0700

    desos

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            c = chr(ord(c) - 32)
-        print("{}".format(c), end="")
-    print()
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
