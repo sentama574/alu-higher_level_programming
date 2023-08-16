27c590fabb10be6d5ee969daa06664a551d515bc
sentama574 <sentamaj54@gmail.com>
2023-08-16 12:31:14 -0700

    desos

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Adds the State object "Louisiana" to the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == '__main__':
+    # Create a connection to the database
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'.
+        format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Add a new State object to the database
+    new_state = State(name='Louisiana')
+    session.add(new_state)
+    session.commit()
+
+    # Print the new state's id
+    print(new_state.id)
